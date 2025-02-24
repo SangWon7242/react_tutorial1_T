220416bd1da639fa9487c6fe8b3e02b184118e91
SangWon7242 <gotkdqja@gamil.com>
2025-02-24 11:35:14 +0900

    useState를 이용한 숫자 증가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import React, { useState } from "react";
+
 export default function Home() {
-  return <div>테스트</div>;
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div>
+      <div>{number}</div>
+      <button onClick={() => setNumber(number + 1)}>증가</button>
+      &nbsp;
+      <button onClick={() => setNumber(number - 1)}>감소</button>
+    </div>
+  );
 }
