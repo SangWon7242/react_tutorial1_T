a93e534c499ee466d480b4f34089d8c723424ffc
SangWon7242 <gotkdqja@gamil.com>
2025-03-04 12:18:09 +0900

    숫자 기록과 리스트를 별도의 컴포넌트로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,62 @@
 "use client";
 import React, { useState } from "react";
 
+interface NumberControllerProps {
+  number: number;
+  increaseNumber: () => void;
+  decreaseNumber: () => void;
+  saveNumber: () => void;
+  resetNumber: () => void;
+}
+
+interface RecordedNumbersListProps {
+  numberList: number[];
+}
+
+const NumberController = ({
+  number,
+  increaseNumber,
+  decreaseNumber,
+  saveNumber,
+  resetNumber,
+}: NumberControllerProps) => {
+  return (
+    <div>
+      <div>{number}</div>
+      <button onClick={increaseNumber}>증가</button>
+      &nbsp;
+      <button onClick={decreaseNumber}>감소</button>
+      &nbsp;
+      <button onClick={saveNumber}>기록</button>
+      &nbsp;
+      <button onClick={resetNumber}>리셋</button>
+    </div>
+  );
+};
+
+const RecordedNumbersList = ({ numberList }: RecordedNumbersListProps) => {
+  return (
+    <div>
+      {numberList.length == 0 ? (
+        <h1>숫자를 기록해주세요.</h1>
+      ) : (
+        <>
+          <h1>기록 된 숫자</h1>
+          <nav>
+            <ul>
+              {numberList.map((num, index) => (
+                <li key={index}>
+                  {index + 1}번 : {num}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </>
+      )}
+    </div>
+  );
+};
+
 export default function Home() {
   const [number, setNumber] = useState<number>(0);
   const [numberList, setNumberList] = useState<number[]>([]);
@@ -27,34 +83,14 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <div>{number}</div>
-        <button onClick={increaseNumber}>증가</button>
-        &nbsp;
-        <button onClick={decreaseNumber}>감소</button>
-        &nbsp;
-        <button onClick={saveNumber}>기록</button>
-        &nbsp;
-        <button onClick={resetNumber}>리셋</button>
-      </div>
-      <div>
-        {numberList.length == 0 ? (
-          <h1>숫자를 기록해주세요.</h1>
-        ) : (
-          <>
-            <h1>기록 된 숫자</h1>
-            <nav>
-              <ul>
-                {numberList.map((num, index) => (
-                  <li key={index}>
-                    {index + 1}번 : {num}
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </>
-        )}
-      </div>
+      <NumberController
+        number={number}
+        increaseNumber={increaseNumber}
+        decreaseNumber={decreaseNumber}
+        saveNumber={saveNumber}
+        resetNumber={resetNumber}
+      />
+      <RecordedNumbersList numberList={numberList} />
     </>
   );
 }
