775a32b2f7f07587b4830a995eca458bead9c31b
SangWon7242 <gotkdqja@gamil.com>
2025-03-05 12:08:59 +0900

    공통 header 적용

---
+++ src/app/common/Header.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="top-bar h-20 border-b-2 ">
+      <div className="inner mx-auto flex h-full px-12 ">
+        <nav className="menu-1">
+          <ul className="flex h-full">
+            <li className="nav-item">
+              <Link href="/" className="menu-text">
+                메인 페이지
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/counter" className="menu-text">
+                숫자 기록 앱
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/app/todos/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React, { useState } from "react";
+
+export default function Todo() {
+  return <div>할일 앱</div>;
+}
